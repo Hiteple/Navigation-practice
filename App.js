a1d67721bc94cbd23ed5dafc43ccfc35039e5a95
hiteple <hiteplegold@gmail.com>
2020-02-21 01:11:25 -0300

    Adding more options and icon

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import {Text} from 'react-native';
 import Home from './src/components/Home';
 import Login from './src/components/Login';
 import About from './src/components/About';
 import Profile from './src/components/Profile';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = new createStackNavigator();
+const myIcon = <Icon name="rocket" size={30} color="#900" />;
 
 const App = () => {
   return (
@@ -16,14 +19,22 @@ const App = () => {
         initialRouteName="Home"
         headerMode="screen"
         screenOptions={{
-          headerTintColor: 'white',
-          headerStyle: {backgroundColor: 'tomato'},
+          headerTintColor: '#fff',
+          headerTitleAlign: 'center',
+          //header: () => <Text>This is a header</Text>,
+          headerStyle: {backgroundColor: 'purple'},
+          // Gestures
+          gesturesEnabled: true,
+          //Only in iOS
+          //headerBackTitle: 'Back',
+          headerBackImage: () => myIcon,
         }}>
         <Stack.Screen
           name="Home"
           component={Home}
           options={{
             title: 'This is the Home',
+            headerTitleAllowFontScaling: true,
           }}
         />
         <Stack.Screen name="Login" component={Login} />
